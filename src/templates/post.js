6bebc14350fbb8389d93cff101b807f38c7a6454
AmmyK <kojikeisisu@gmail.com>
2022-09-03 07:29:44 +0900

    fix warn

--- src/templates/post.js
+++ src/templates/post.js
@@ -1,7 +1,7 @@
 import React from "react"
 import "../styles/post.css"
 import Layout from "../components/layout"
-import SEO from "../components/seo"
+import Seo from "../components/seo"
 
 export default function Post({ pageContext }) {
     const { title, updatedAt, image } = pageContext.post;
@@ -10,7 +10,7 @@ export default function Post({ pageContext }) {
 
     return(
         <Layout>
-            <SEO title={title} description={description} />
+            <Seo title={title} description={description} />
             <div className="post-header">
                 <h1>{title}</h1>
                 <p className="post-date">{updatedAt}</p>
